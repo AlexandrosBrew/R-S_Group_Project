1faba2455929da676c9a3b8969e2e7ba15782b90
795302 <795302@as126-10.adir.hull.ac.uk>
2025-03-20 14:32:00 +0000

    Odometry addition

--- ros2_ws/install/GazeboRobotSim/lib/python3.10/site-packages/GazeboRobotSim/move_robot.py
+++ ros2_ws/install/GazeboRobotSim/lib/python3.10/site-packages/GazeboRobotSim/move_robot.py
@@ -1,18 +1,25 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 class MoveRobot(Node):
     def __init__(self):
         super().__init__('move_robot')
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.timer = self.create_timer(0.1, self.move_forward)
-
+        self.subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+	
     def move_forward(self):
         msg = Twist()
         msg.linear.x = 0.2  # Move forward
         msg.angular.z = 0.0  # No rotation
         self.publisher.publish(msg)
+    
+    def odom_callback(self, msg):
+    	position = msg.pose.pose.position
+    	orientation = msg.pose.pose.orientation
+    	self.get_logger().info(f'Position -> x: {position.x}, y: {position.y}, z: {position.z}')
 
 def main():
     rclpy.init()
