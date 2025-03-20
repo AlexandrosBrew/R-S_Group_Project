fa384850e471564006ec2dc616b3384e60e7ca58
795302 <795302@as126-10.adir.hull.ac.uk>
2025-03-20 16:15:43 +0000

    Movement added

--- ros2_ws/src/GazeboRobotSim/GazeboRobotSim/move_robot.py
+++ ros2_ws/src/GazeboRobotSim/GazeboRobotSim/move_robot.py
@@ -1,25 +1,59 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
 from nav_msgs.msg import Odometry
+import math
 
 class MoveRobot(Node):
     def __init__(self):
         super().__init__('move_robot')
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.move_forward)
-        self.subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
-	
-    def move_forward(self):
-        msg = Twist()
-        msg.linear.x = 0.2  # Move forward
-        msg.angular.z = 0.0  # No rotation
-        self.publisher.publish(msg)
+        #self.timer = self.create_timer(0.1, self.move_forward)
+        #self.pose_subscription = self.create_subscription(Pose, '/pose', self.pose_callback, 10)
+        self.odom_subscription = self.create_subscription(Odometry, '/odom', self.move_forward, 10)
+        #self.timer = self.create_timer(0.1, self.move_forward)
+        
+    def move_forward(self, msg):       
+        goalx = 1
+        goaly = 1
+        movement = Twist()
+        
+        distanceToGoal = math.sqrt( (goalx - msg.pose.pose.position.x)**2 + (goaly - msg.pose.pose.position.y)**2)
+        
+        angleToGoal = math.atan2( goaly - msg.pose.pose.position.y , goalx - msg.pose.pose.position.x)
+        
+        distanceTolerance = 0.1
+        angleTolerance = 0.01
+        
+        orientation = msg.pose.pose.orientation
+        yaw = math.atan2(2.0 * (orientation.w*orientation.z + orientation.x*orientation.y), 1.0 - 2.0 * (orientation.y*orientation.y + orientation.z*orientation.z))
+        angleDiff = angleToGoal - yaw
+        
+        if angleDiff > math.pi:
+        	angleDiff -= 2*math.pi
+        elif angleDiff < -math.pi:
+        	angleDiff += 2*math.pi
+        
+
+        #angleError = angleToGoal - yaw Might be wrong
+        #self.get_logger().info(f'Angle Error: {angleError}, Yaw: {yaw}, AngleToGoal: {angleToGoal}')
+        
+
+        if abs(angleDiff) > angleTolerance:
+        	movement.angular.z = float(0.3 * angleDiff)
+        else:
+        	movement.angular.z = movement.angular.z
+        
+        if distanceToGoal >= distanceTolerance:
+       		movement.linear.x = 1.1
+        else:
+       		movement.linear.x = 0.0
+       		quit()
+        
+        
+        self.publisher.publish(movement)
     
-    def odom_callback(self, msg):
-    	position = msg.pose.pose.position
-    	orientation = msg.pose.pose.orientation
-    	self.get_logger().info(f'Position -> x: {position.x}, y: {position.y}, z: {position.z}')
 
 def main():
     rclpy.init()
