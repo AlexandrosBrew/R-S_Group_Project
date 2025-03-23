b229ca9056e304ed57126f2988ab1c85fb1e55c2
ragingbeastz <dimithriweera@gmail.com>
2025-03-23 18:08:20 +0000

    Added robot functionality to move and stop at goal

--- ros2_ws/src/GazeboRobotSim/GazeboRobotSim/move_robot.py
+++ ros2_ws/src/GazeboRobotSim/GazeboRobotSim/move_robot.py
@@ -9,51 +9,52 @@ class MoveRobot(Node):
     def __init__(self):
         super().__init__('move_robot')
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        #self.timer = self.create_timer(0.1, self.move_forward)
-        #self.pose_subscription = self.create_subscription(Pose, '/pose', self.pose_callback, 10)
         self.odom_subscription = self.create_subscription(Odometry, '/odom', self.move_forward, 10)
-        #self.timer = self.create_timer(0.1, self.move_forward)
         
     def move_forward(self, msg):       
         goalx = 1
         goaly = 1
         movement = Twist()
         
+        #Get distance to goal
         distanceToGoal = math.sqrt( (goalx - msg.pose.pose.position.x)**2 + (goaly - msg.pose.pose.position.y)**2)
         
-        angleToGoal = math.atan2( goaly - msg.pose.pose.position.y , goalx - msg.pose.pose.position.x)
-        
-        distanceTolerance = 0.1
-        angleTolerance = 0.01
-        
+        #Get angle to goal
+        angleToGoal = math.atan2( goaly - msg.pose.pose.position.y , goalx - msg.pose.pose.position.x)        
+
         orientation = msg.pose.pose.orientation
-        yaw = math.atan2(2.0 * (orientation.w*orientation.z + orientation.x*orientation.y), 1.0 - 2.0 * (orientation.y*orientation.y + orientation.z*orientation.z))
-        angleDiff = angleToGoal - yaw
+        yaw = math.atan2(2.0 * (orientation.w*orientation.z + orientation.x*orientation.y), 1.0 - 2.0 * (orientation.y*orientation.y + orientation.z*orientation.z)) #Get yaw angle from quaternion
         
-        if angleDiff > math.pi:
-        	angleDiff -= 2*math.pi
-        elif angleDiff < -math.pi:
-        	angleDiff += 2*math.pi
-        
+        #Adjust yaw and angleToGoal angles to be between 0 and 2pi
+        yaw += math.pi
+        angleToGoal += math.pi
 
-        #angleError = angleToGoal - yaw Might be wrong
-        #self.get_logger().info(f'Angle Error: {angleError}, Yaw: {yaw}, AngleToGoal: {angleToGoal}')
-        
+        #Movement Parameters
+        turningSpeed = 0.2
+        drivingSpeed = 0.5
 
-        if abs(angleDiff) > angleTolerance:
-        	movement.angular.z = float(0.3 * angleDiff)
+        angleAccuracy = 0.5
+        distanceAccuracy = 0.1
+
+        angleDifference  = abs(yaw - angleToGoal)
+
+        if yaw < (angleToGoal - angleAccuracy) or yaw > (angleToGoal + angleAccuracy):
+            movement.angular.z = float(turningSpeed * angleDifference)
+            message = "Turning"
         else:
-        	movement.angular.z = movement.angular.z
-        
-        if distanceToGoal >= distanceTolerance:
-       		movement.linear.x = 1.1
-        else:
-       		movement.linear.x = 0.0
-       		quit()
-        
-        
+            movement.angular.z = 0.0
+            message = "Aligned"
+            if distanceToGoal > distanceAccuracy:
+                movement.linear.x = float(drivingSpeed*distanceToGoal*-1) #Robot moving in reverse as it is facing the wrong way
+                message = "Aligned and Moving"
+            else:
+                movement.linear.x = 0.0
+                message = "At Goal"
+
+        self.get_logger().info(f'Current yaw: {yaw} Goal Angle: {angleToGoal} message: {message}') 
         self.publisher.publish(movement)
     
+
 
 def main():
     rclpy.init()
